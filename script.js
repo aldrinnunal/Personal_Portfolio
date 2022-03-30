9fa985c2ccb6c0e1ba43bf46a52f727ef382a354
aldrin24 <ramoresaldrinpup@gmail.com>
2022-03-31 01:50:14 +0800

    asd

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // TOGGLE MENU MOBILE
-
 var toggle = document.querySelector('#toggleBar');
 var overlay = document.querySelector('.overlay');
 var mobilelinks = document.querySelector('.mobilelinks');
+
 toggle.addEventListener('click',function(){
+
     if (toggle.classList.contains('open')){
         toggle.classList.remove('open');
         overlay.style.display = "none";
@@ -39,9 +40,25 @@ window.addEventListener('scroll',function(){
     var header = document.querySelector('header');
     if(window.scrollY==0){
         header.style.backgroundColor = "transparent";
+        header.style.boxShadow = "none";
     }
     else{
         header.style.backgroundColor = "rgb(15, 15, 15)";
+        header.style.boxShadow = "0px 4px 3px rgb(15, 15, 15)";
  
     }
 });
+
+// TYPING EFFECT
+var i  = 0;
+var speed = 40;
+var txt = "ASPIRING UI/UX ENGINEER";
+TypingEffect();
+
+function TypingEffect(){
+    if (i < txt.length){
+        document.getElementById('typeEffect').innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(TypingEffect, speed);
+    }
+};
